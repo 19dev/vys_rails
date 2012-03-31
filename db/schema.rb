d66ca5fb82fe7e61796f00d898ca352a5dadaf2a
bsaral <betul.saral@bil.omu.edu.tr>
2012-03-31 03:49:23 +0300

    db:seed.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329225000) do
+ActiveRecord::Schema.define(:version => 20120329233536) do
 
   create_table "forms", :force => true do |t|
     t.string   "name"
